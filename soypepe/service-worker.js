997e599c854a9656c33f25b1706b2259f261b9e0
javi9009 <javi@casapepe.mx>
2026-06-24 21:42:23 -0600

    Add files via upload

--- soypepe/service-worker.js
+++ soypepe/service-worker.js
@@ -2,7 +2,7 @@
  * Estrategia: network-first para no servir datos viejos; cae al caché offline.
  * Cachea el cascarón (shell) para que la PWA sea instalable y abra sin conexión.
  */
-const CACHE = 'soypepe-shell-v17';
+const CACHE = 'soypepe-shell-v18';
 const SHELL = [
   './',
   './index.html',
